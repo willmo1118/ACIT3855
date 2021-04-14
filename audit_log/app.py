286b22b162f6729427f9cea951866e14f1a02af9
willmo1118 <55708084+willmo1118@users.noreply.github.com>
2021-04-14 09:52:57 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -98,8 +98,10 @@ def get_payment(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.add_api("openapi.yaml", base_path="/audit_log", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":

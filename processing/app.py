50e9c6a725e1caeb2160198e1838facf7487a23b
willmo1118 <55708084+willmo1118@users.noreply.github.com>
2021-04-14 09:53:27 -0700

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -124,8 +124,9 @@ def init_scheduler():
     sched.start()
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS'] = 'Content-Type'
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS'] = 'Content-Type'
 app.add_api("openapi.yaml", base_path="/processing", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
